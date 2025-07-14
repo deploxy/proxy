ef182b7db48be57bd703a94fcde45813d6117756
J222 <baram789456@gmail.com>
2025-07-14 17:11:51 +0900

    🔊 add log

--- src/lib/proxy-url.ts
+++ src/lib/proxy-url.ts
@@ -11,21 +11,31 @@ export function buildProxyUrl({
   headers: Record<string, any>
   logger: Logger
 }) {
-  const deploxyRegion = headers['x-deploxy-region']
-  const deploxyPkgId = headers['x-deploxy-pkg-id']
+  try {
+    const deploxyRegion = headers['x-deploxy-region']
+    const deploxyPkgId = headers['x-deploxy-pkg-id']
 
-  if (!deploxyRegion || !deploxyPkgId) {
-    const proxyUrl = new URL(path, baseUrl).href
-    logger.info(`proxyHref: ${proxyUrl}`)
-    return proxyUrl
+    logger.info(`Building proxy URL with deploxyRegion: ${deploxyRegion}`)
+    logger.info(`Building proxy URL with deploxyPkgId: ${deploxyPkgId}`)
+
+    if (!deploxyRegion || !deploxyPkgId) {
+      const proxyUrl = new URL(path, baseUrl).href
+      logger.info(`proxyHref: ${proxyUrl}`)
+      return proxyUrl
+    }
+
+    // Ensure remove double slash
+    const proxyPath = path.startsWith('/') ? path.slice(1) : path
+    logger.info(`Building proxy URL with proxyPath: ${proxyPath}`)
+    const deploxyProxyUrl = new URL(
+      `/${deploxyPkgId}/${deploxyRegion}/${proxyPath}`,
+      baseUrl,
+    ).href
+    logger.info(`deploxyHref: ${deploxyProxyUrl}`)
+    return deploxyProxyUrl
+  } catch (err) {
+    logger.error(`Error building proxy URL`)
+    logger.error(JSON.stringify(err, null, 2))
+    throw err
   }
-
-  // Ensure remove double slash
-  const proxyPath = path.startsWith('/') ? path.slice(1) : path
-  const deploxyProxyUrl = new URL(
-    `/${deploxyPkgId}/${deploxyRegion}/${proxyPath}`,
-    baseUrl,
-  ).href
-  logger.info(`deploxyHref: ${deploxyProxyUrl}`)
-  return deploxyProxyUrl
 }
